fbb29af38a92ce1b56fe6409431f249be50a7658
K.H.Park <svclaw2000@gmail.com>
2021-09-05 14:33:32 +0900

    chore: Add data binding

--- app/src/main/java/com/khnsoft/follownumber/game/view/GameActivity.kt
+++ app/src/main/java/com/khnsoft/follownumber/game/view/GameActivity.kt
@@ -2,11 +2,15 @@ package com.khnsoft.follownumber.game.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.khnsoft.follownumber.R
+import com.khnsoft.follownumber.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_game)
     }
 }
--- app/src/main/java/com/khnsoft/follownumber/main/view/MainActivity.kt
+++ app/src/main/java/com/khnsoft/follownumber/main/view/MainActivity.kt
@@ -4,15 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import com.khnsoft.follownumber.R
+import com.khnsoft.follownumber.databinding.ActivityMainBinding
 import com.khnsoft.follownumber.game.view.GameActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<TextView>(R.id.tvStartGame).setOnClickListener {
+        binding.tvStartGame.setOnClickListener {
             startActivity(Intent(this, GameActivity::class.java))
         }
     }
