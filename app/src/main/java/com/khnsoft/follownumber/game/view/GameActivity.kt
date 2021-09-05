fc7fa6e23f6358cd0cd1cd2dec0f07b771c186bc
K.H.Park <svclaw2000@gmail.com>
2021-09-05 14:16:09 +0900

    feat: Add empty game activity

---
+++ app/src/main/java/com/khnsoft/follownumber/game/view/GameActivity.kt
@@ -0,0 +1,12 @@
+package com.khnsoft.follownumber.game.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.khnsoft.follownumber.R
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
